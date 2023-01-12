4d63ebd9bb411bba53f64b655a27f52bcb2ca47c
flos-flower <flosmeansflower@gmail.com>
2023-01-12 18:50:56 +0500

    Started changing the app

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -148,24 +148,33 @@ class HomePage extends React.Component {
           </div>
           <div id="list-wrapper">
             {tasks.map((task, index)=>{
-              return(
+              // return(
+              //   <div key={index} className="task-wrapper flex-wrapper">
+
+              //     <div onClick={() => self.strikeUnstrike(task)} style={{flex:7}}>
+              //       {task.completed === false ? (
+              //         <span>{task.title}</span>
+              //       ) : (
+              //         <strike>{task.title}</strike>
+              //       )}
+              //     </div>
+
+              //     <div style={{flex:1}}>
+              //       <button onClick={() => self.startEdit(task)} className="btn btn-sm btn-outline-info">Edit</button>
+              //     </div>
+
+              //     <div style={{flex:1}}>
+              //       <button onClick={() => self.deleteItem(task)} className="btn btn-sm btn-outline-dark delete">-</button>
+              //     </div>
+              //   </div>
+              // )
+              return (
                 <div key={index} className="task-wrapper flex-wrapper">
-
-                  <div onClick={() => self.strikeUnstrike(task)} style={{flex:7}}>
-                    {task.completed === false ? (
-                      <span>{task.title}</span>
-                    ) : (
-                      <strike>{task.title}</strike>
-                    )}
-                  </div>
-
-                  <div style={{flex:1}}>
-                    <button onClick={() => self.startEdit(task)} className="btn btn-sm btn-outline-info">Edit</button>
-                  </div>
-
-                  <div style={{flex:1}}>
-                    <button onClick={() => self.deleteItem(task)} className="btn btn-sm btn-outline-dark delete">-</button>
-                  </div>
+                  <div>
+                    <div>
+                      
+                    </div>
+                  </div>                  
                 </div>
               )
             }) }
