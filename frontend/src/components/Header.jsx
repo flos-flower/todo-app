f417012bd877c2cc9697b4598c87f122ee2b576e
flos-flower <flosmeansflower@gmail.com>
2023-02-25 17:00:21 +0500

    Added some functionality to table select

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -12,6 +12,7 @@ const Header = () => {
   let [tableSelectVisible, setTableSelectVisible] = useState(false);
 
   const ref = useRef(null);
+  const tableRef = useRef(null);
 
   let changeVisibility = () => {
     setDrop(!drop);
@@ -33,89 +34,102 @@ const Header = () => {
     };
   }, [changeVisibility]);
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (tableRef.current && !tableRef.current.contains(event.target) && tableSelectVisible === true) {
+        changeSelectVisibility && changeSelectVisibility();
+      }
+    };
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [changeSelectVisibility]);
 
   if (tableList !== undefined)
-  return (
-    <div className={s.headerDiv}>
-      {window.location.href === "http://localhost:3000/" ? (
-        <div className={`${s.homeBtn} ${s.selected}`}>
-          <Link to="/">Home</Link>
-        </div>
-      ) : (
-        <div className={s.homeBtn}>
-          <Link to="/">Home</Link>
-        </div>
-      )}
-      {user ? (
-        <div className={s.profileDiv}>
-          <div
-            className={s.selectTable}
-            onClick={() => {
-              setTableSelectVisible(!tableSelectVisible);
-            }}
-          >
-            <span className={s.selectedOption}>{tableList[0].name}</span>
-            <ul className={s.tableOptions}>
-              {tableSelectVisible &&
-                tableList.map((table, index) => {
-                  return <li key={index}>{table.name}</li>;
-                })}
-            </ul>
+    return (
+      <div className={s.headerDiv}>
+        {window.location.href === "http://localhost:3000/" ? (
+          <div className={`${s.homeBtn} ${s.selected}`}>
+            <Link to="/">Home</Link>
           </div>
-          <div className={s.dropdownContainer} ref={ref}>
-            {profile !== undefined ? (
-              <img
-                className={s.profileImage}
-                src={`http://127.0.0.1:8000/Programming/DJ and ReactJS/todo-app/todo/media${profile[0].picture}`}
-                alt="Profile"
-                onClick={() => {
-                  setDrop(!drop);
-                }}
-              />
+        ) : (
+          <div className={s.homeBtn}>
+            <Link to="/">Home</Link>
+          </div>
+        )}
+        {user ? (
+          <div className={s.profileDiv}>
+            <div
+              className={s.selectTable}
+              onClick={() => {
+                setTableSelectVisible(!tableSelectVisible);
+              }}
+              ref={tableRef}
+            >
+              <span className={s.selectedOption}>{tableList[0].name}</span>
+              {tableSelectVisible && (
+                <ul className={s.tableOptions}>
+                  {tableList.map((table, index) => {
+                    return <li key={index}>{table.name}</li>;
+                  })}
+                </ul>
+              )}
+            </div>
+            <div className={s.dropdownContainer} ref={ref}>
+              {profile !== undefined ? (
+                <img
+                  className={s.profileImage}
+                  src={`http://127.0.0.1:8000/Programming/DJ and ReactJS/todo-app/todo/media${profile[0].picture}`}
+                  alt="Profile"
+                  onClick={() => {
+                    setDrop(!drop);
+                  }}
+                />
+              ) : (
+                <img
+                  className={s.profileImage}
+                  alt="Profile"
+                  onClick={() => {
+                    setDrop(!drop);
+                  }}
+                />
+              )}
+              {drop && (
+                <ProfileDropdown
+                  user={user}
+                  changeVisibility={() => {
+                    setDrop(!drop);
+                  }}
+                  logoutUser={logoutUser}
+                />
+              )}
+            </div>
+          </div>
+        ) : (
+          <div className={s.authDiv}>
+            {window.location.href === "http://localhost:3000/login" ? (
+              <div className={s.selected}>
+                <Link to="/login">Login</Link>
+              </div>
             ) : (
-              <img
-                className={s.profileImage}
-                alt="Profile"
-                onClick={() => {
-                  setDrop(!drop);
-                }}
-              />
+              <div>
+                <Link to="/login">Login</Link>
+              </div>
             )}
-            {drop && (
-              <ProfileDropdown
-                user={user}
-                changeVisibility={() => {
-                  setDrop(!drop);
-                }}
-                logoutUser={logoutUser}
-              />
+            {window.location.href === "http://localhost:3000/register" ? (
+              <div className={s.selected}>
+                <Link to="/register">Register</Link>
+              </div>
+            ) : (
+              <div>
+                <Link to="/register">Register</Link>
+              </div>
             )}
           </div>
-        </div>
-      ) : (
-        <div className={s.authDiv}>
-          {window.location.href === "http://localhost:3000/login" ? (
-            <div className={s.selected}>
-              <Link to="/login">Login</Link>
-            </div>
-          ) : (
-            <div>
-              <Link to="/login">Login</Link>
-            </div>
-          )}
-          {window.location.href === "http://localhost:3000/register" ? (
-            <div className={s.selected}>
-              <Link to="/register">Register</Link>
-            </div>
-          ) : (
-            <div>
-              <Link to="/register">Register</Link>
-            </div>
-          )}
-        </div>
-      )}
-    </div>
-  );
+        )}
+      </div>
+    );
 };
 
 export default Header;
